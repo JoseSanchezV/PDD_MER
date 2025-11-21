d1a0cb5e39f804198824b45bc4fec3173d6bd7c2
JoseSanchezV <jose.sanchez.villa@gmail.com>
2025-11-21 09:17:30 -0600

    Update updater.bat

--- app_updates/updater.bat
+++ app_updates/updater.bat
@@ -3,29 +3,28 @@ echo Iniciando proceso de actualizacion. NO CIERRE esta ventana.
 timeout /t 3 /nobreak > nul
 
 :: 1. Define los nombres de los archivos
-:: **ASEGURATE que OLD_EXE coincida con NOMBRE_EXE_PRINCIPAL en Python**
+:: Nombre del .exe que se va a reemplazar (nombre_de_tu_app.exe)
 set "OLD_EXE=PDD_MER.exe"
-:: **ASEGURATE que NEW_EXE coincida con NOMBRE_EXE_TEMPORAL en Python**
-set "NEW_EXE=app_new.exe"
+:: Nombre del .exe descargado del Release (app_new.exe)
+set "NEW_EXE=PDD_MER_new.exe"
 set "BAT_FILE=updater.bat"
 
 echo Intentando eliminar el archivo antiguo...
 
-:: 2. Intentar eliminar el ejecutable viejo (liberado por Python)
+:: 2. Intentar eliminar el ejecutable viejo 
 del "%OLD_EXE%"
 
 :: 3. Verificar si la eliminacion fue exitosa
 if not exist "%OLD_EXE%" (
     echo Reemplazando el ejecutable...
-    :: Mover/Renombrar el nuevo archivo al nombre del viejo
+    :: Renombrar el nuevo archivo al nombre del viejo
     rename "%NEW_EXE%" "%OLD_EXE%"
     echo Lanzando la nueva version...
-    :: Iniciar la nueva aplicacion y continuar
+    :: Iniciar la nueva aplicacion
     start "" "%OLD_EXE%"
 ) else (
     echo.
     echo ERROR CRITICO: No se pudo eliminar el ejecutable anterior (%OLD_EXE%).
-    echo Por favor, cierre todos los procesos relacionados y reinicie la aplicacion.
     pause
 )
 
